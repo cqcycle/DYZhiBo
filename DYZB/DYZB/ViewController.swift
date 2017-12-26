cac17bf71bf7a609854844f353075e04cc248515
ddy <15696564260@163.com>
2017-12-26 14:27:34 +0800

    提交

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,31 @@
+
+
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by ddy on 2017/12/26.
+//  Copyright © 2017年 Cycle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("welcome to swift")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
